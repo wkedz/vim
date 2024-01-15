56301193a7f5b5389e4529cb54171da2ccdb9209
Wiesław Kędzierski <wkedzierski@gmail.com>
2024-01-15 10:16:04 +0100

    Split nvim config into seperate files.

---
+++ nvim-setup/config/lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter", 
+        build = ":TSUpdate",
+        config = function()
+            -- load config table from nvim-treesitter
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    -- default ones, suggested by treesitter
+                    "c", "lua", "vim", "vimdoc", "query", 
+                    -- added by me 
+                    "go", "python", "cpp"  
+                },
+                -- auto install missing parsers 
+                auto_install = true,
+
+                -- enable highlighting
+                highlight = {
+                    enable = true,
+                },
+            })
+        end,
+    }
+}
